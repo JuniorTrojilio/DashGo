96733e4925bf810454776835a169fcf69cd2b1fc
Junior Trojilio <osmar.trojilio@gmail.com>
2021-08-09 23:47:21 -0300

    feat: add features to appear color in mobile devices

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" /> 
           <link rel="preconnect" href="https://fonts.googleapis.com" /> 
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="#1f2029" />
+          <meta name="theme-color" content="#1f2029" />
         </Head>
         <body>
           <Main />
