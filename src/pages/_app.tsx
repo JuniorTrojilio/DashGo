014f685a57ca6835fc76251ff96fddaf0aae732a
Junior Trojilio <osmar.trojilio@gmail.com>
2021-08-11 18:02:15 -0300

    feat: add auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,21 +6,24 @@ import { makeServer } from '../services/mirage'
 import { QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { queryClient } from '../services/queryClient'
+import { AuthProvider } from '../contexts/AuthContext'
 
-if(process.env.NODE_ENV === 'development') {
-  makeServer()
-}
+// if(process.env.NODE_ENV === 'development') {
+//   makeServer()
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <SidebarDrawerProvider>
-          <Component {...pageProps} />
-        </SidebarDrawerProvider>
-      </ChakraProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <SidebarDrawerProvider>
+            <Component {...pageProps} />
+          </SidebarDrawerProvider>
+        </ChakraProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </AuthProvider>
   )
 }
 
